7945da376a4e3f3b6ef61e3b15ee34b0653bd9ab
PhoenixJack <35715826+Phoenixjack@users.noreply.github.com>
2026-06-14 15:54:27 -0400

    Replace sprintf with snprintf for safer string formatting

--- serial_buffer.ino
+++ serial_buffer.ino
@@ -17,19 +17,17 @@ void loop() {
   if (port0.check() || port1.check() || port2.check()) {
     digitalWrite(LED_BUILTIN, HIGH);
     if (port0.is_Buffer_Ready_To_Send()) {
-      sprintf(printbuffer, "PORT0 (%03.1f Kbps) START: %i \t STOP: %i \t", ((float)port0.get_baud_rate() / 1000), port0.get_start_time(), port0.get_stop_time());
-      strcat(printbuffer, port0.buffer);
+      snprintf(printbuffer, sizeof(printbuffer), "PORT0 (%03.1f Kbps) START: %lu \t STOP: %lu \t%s", ((float)port0.get_baud_rate() / 1000), (unsigned long)port0.get_start_time(), (unsigned long)port0.get_stop_time(), port0.buffer);
       Serial.println(printbuffer);
       port0.reset();
     }
     if (port1.is_Buffer_Ready_To_Send()) {
-      sprintf(printbuffer, "PORT1 (%03.1f Kbps) START: %i \t STOP: %i \t", ((float)port1.get_baud_rate() / 1000), port1.get_start_time(), port1.get_stop_time());
-      strcat(printbuffer, port1.buffer);
+      snprintf(printbuffer, sizeof(printbuffer), "PORT1 (%03.1f Kbps) START: %lu \t STOP: %lu \t%s", ((float)port1.get_baud_rate() / 1000), (unsigned long)port1.get_start_time(), (unsigned long)port1.get_stop_time(), port1.buffer);
       Serial.println(printbuffer);
       port1.reset();
     }
     if (port2.is_Buffer_Ready_To_Send()) {
-      sprintf(printbuffer, "PORT2 (%000.1f Kbps) START: %i \t STOP: %i \t", ((float)port2.get_baud_rate() / 1000), port2.get_start_time(), port2.get_stop_time());
+      snprintf(printbuffer, sizeof(printbuffer), "PORT2 (%03.1f Kbps) START: %lu \t STOP: %lu \t%s", ((float)port2.get_baud_rate() / 1000), (unsigned long)port2.get_start_time(), (unsigned long)port2.get_stop_time(), port2.buffer);
       Serial.println(printbuffer);
       port2.Send_Buffer(&Serial);
       Serial.println("");
